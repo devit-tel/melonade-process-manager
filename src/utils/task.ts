2178e8a433e1962e95c9824c361ec3acc960952a
Spz.Takumi <admin@4th.in.th>
2020-05-12 11:09:10 +0700

    Added boolean, math, compare  operator

--- src/utils/task.ts
+++ src/utils/task.ts
@@ -9,6 +9,68 @@ export const getPathByInputTemplate = R.compose(
   R.replace(/(^\${)(.+)(}$)/i, '$2'),
 );
 
+const expressionOperators = {
+  '==': (a: any, b: any) => { return a == b },
+  '!=': (a: any, b: any) => { return a != b },
+  '<': (a: any, b: any) => { return a < b },
+  '>': (a: any, b: any) => { return a > b },
+  '<=': (a: any, b: any) => { return a <= b },
+  '>=': (a: any, b: any) => { return a >= b },
+  '&&': (a: any, b: any) => { return a && b },
+  '||': (a: any, b: any) => { return a || b },
+  '+': (a: any, b: any) => { return a + b },
+  '-': (a: any, b: any) => { return a - b },
+  '*': (a: any, b: any) => { return a * b },
+  '/': (a: any, b: any) => { return a / b },
+};
+
+
+const solveRegExp = [ // Order by operator priority
+  /\&\&|\|\|/g, 
+  /==|!=|>=|<=|>|</g, 
+  /\+|\-/g, 
+  /\*|\//g, 
+];
+
+
+
+const solveExpression = (expression: string, values: any, depth: number = 0) => {
+
+  if (depth == solveRegExp.length) { // No more operator to solve just resolve variable
+    const trimmedExpression = expression.trim();
+    if (/^\d+$/.test(trimmedExpression)) { // Check if it's number
+      return parseInt(trimmedExpression);
+    }
+
+    if (/^\${[a-z0-9-_.\[\]]+}$/i.test(trimmedExpression)) { // Check if it's variable
+      return _.get(trimmedExpression.replace(/(^\${)(.+)(}$)/i, '$2'), values);
+    }
+    return expression; //Assume it's string Do not trim the string
+  }
+
+  const matchRegExp = solveRegExp[depth];
+  const operators = expression.match(matchRegExp);
+  const operands = expression.replace(matchRegExp, "&&").split("&&"); // Replace everything to && to split it properly
+  if (operators) {
+    if (
+      operators.length > 0 &&
+      operands.length > 0 &&
+      operators.length == (operands.length - 1)
+    ) {
+      let result = solveExpression(operands[0], values, depth + 1);
+      for (let i = 0; i < operators.length; i++) {
+        let nextOperand = solveExpression(operands[i + 1], values, depth + 1);
+        result = expressionOperators[operators[i].trim()](result, nextOperand);
+      }
+      return result;
+    } else {
+      return expression;
+    }
+  } else {
+    return solveExpression(expression, values, depth + 1);
+  }
+}
+
 // Support 2 types of template
 const parseTemplate = (template: string, values: any) => {
   // query template
@@ -21,6 +83,12 @@ const parseTemplate = (template: string, values: any) => {
   // ${parcel[0].driverId} => "driver_1"
   if (/^\${[a-z0-9-_.\[\]]+}$/i.test(template)) {
     return _.get(template.replace(/(^\${)(.+)(}$)/i, '$2'), values);
+  }
+
+  // expression template
+  // ${${parcel[0].driverId} == ${parcel[0].driverId}}
+  if (/(^\${)(.+)(}$)/i.test(template)) {
+    return solveExpression(template.replace(/(^\${)(.+)(}$)/i, '$2'), values);
   }
 
   return template;
